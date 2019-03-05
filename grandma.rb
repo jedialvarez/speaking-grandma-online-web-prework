3dfcb9feeee564906ff08a36d0fb7ab6de910a22
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-03-05 19:12:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,9 @@
 # Write a speak_to_grandma method.
-if speak_to_grandma("no caps")
+
+def speak_to_grandma(string)
   puts "HUH?! SPEAK UP, SONNY!"
+end
+  
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
