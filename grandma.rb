715912845b348780371e703740238da47adf8c2d
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-03-05 19:23:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,6 +5,8 @@ def speak_to_grandma(string)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif string == "Hi!"
     return "HUH?! SPEAK UP, SONNY!"
+  elsif string == "Hi Nana, how are you?"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
